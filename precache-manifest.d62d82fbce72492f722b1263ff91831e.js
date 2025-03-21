7cfa0e9edda475066d1a7b959aee2e7fcb16178c
Miles Lilly <mileslilly2@gmail.com>
2025-03-21 13:43:01 -0400

    Updates

---
+++ precache-manifest.d62d82fbce72492f722b1263ff91831e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc86609ab05032c7c55bbbf54c0eadc6",
+    "url": "/trivia-react/index.html"
+  },
+  {
+    "revision": "b786b0bb62cc99388825",
+    "url": "/trivia-react/static/css/main.915a252e.chunk.css"
+  },
+  {
+    "revision": "638e200e69d950262938",
+    "url": "/trivia-react/static/js/2.ae026ab9.chunk.js"
+  },
+  {
+    "revision": "b786b0bb62cc99388825",
+    "url": "/trivia-react/static/js/main.1df5ea49.chunk.js"
+  },
+  {
+    "revision": "363ee4982c5f28a3f23d",
+    "url": "/trivia-react/static/js/runtime~main.9c4492c5.js"
+  }
+]);
